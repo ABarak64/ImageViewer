017348c25af7c7fc573ab6d6ef2af224c9b032b4
Adam Barak <adam.barak.64@gmail.com>
2012-09-15 13:09:29 -0700

    -Working project that sources Reddit for pictures, but some cleanup needs to be done.

---
+++ ImageViewer/ImageProviders/RedditPage.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Reflection;
+using System.Net;
+using System.Net.Cache;
+
+namespace ImageViewer.ImageProviders
+{
+    /// <summary>
+    /// A Reddit page model that consumes the Reddit page Json API.
+    /// </summary>
+    class RedditPage : IImageUrlProvider
+    {
+        private readonly dynamic _redditPage;
+
+        /// <summary>
+        /// Constructing the object with a Uri downloads the Json content of that page in a blocking manner.
+        /// </summary>
+        /// <param name="redditPageUrl">The url to a page of a Reddit board.</param>
+        public RedditPage(Uri redditPageUrl)
+        {
+            // If the passed url doesn't have .json appended, then append it so we can grab the Json for the page.
+            if (!redditPageUrl.AbsoluteUri.EndsWith(".json"))
+            {
+                redditPageUrl = new Uri(redditPageUrl.AbsoluteUri + ".json");
+            }
+            // Download the page Json.
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Proxy = null;  //avoids dynamic proxy discovery delay
+                webClient.CachePolicy = new RequestCachePolicy(RequestCacheLevel.Default);
+                byte[] redditJson = null;
+
+                redditJson = webClient.DownloadData(redditPageUrl);
+                if (redditJson == null)
+                {
+                    throw new ApplicationException("No data found at requested url.");
+                }
+ 
+                var jss = new JavaScriptSerializer();
+                _redditPage = jss.Deserialize<Dictionary<string, dynamic>>(Encoding.UTF8.GetString(redditJson));
+            }
+        }
+
+        /// <summary>
+        /// Construcing a Reddit page when the Json string is already available.
+        /// </summary>
+        /// <param name="redditPageJson"></param>
+        public RedditPage(string redditPageJson)
+        {
+            var jss = new JavaScriptSerializer();
+            _redditPage = jss.Deserialize<Dictionary<string, dynamic>>(redditPageJson);
+        }
+
+        /// <summary>
+        /// Gets a random image url from the Reddit page.
+        /// </summary>
+        /// <returns>A Uri containing a link to an image.</returns>
+        public Uri GetRandomImageUrl()
+        {
+            var posturls = this.PostUrls;
+            var imgUrls = PostUrls.Where(url => url.AbsoluteUri.EndsWith(".jpg"));
+            if (imgUrls.Any())
+            {
+                return imgUrls.ElementAt(new Random().Next(0, imgUrls.Count()));
+            }
+            else
+            {
+                throw new ApplicationException("No image urls found on Reddit page.");
+            }
+        }
+
+        /// <summary>
+        /// Returns the urls of all the posts on this Reddit page.
+        /// </summary>
+        public IEnumerable<Uri> PostUrls
+        {
+            get
+            {
+                ArrayList posts = _redditPage["data"]["children"];
+                return posts.Cast<dynamic>().Select(post => new Uri(post["data"]["url"]));
+            }
+        }
+    }
+}
--- ImageViewer/MainWindow.xaml.cs
+++ ImageViewer/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.IO;
 using System.Net;
 using System.Net.Cache;
+using ImageViewer.ImageProviders;
 
 namespace ImageViewer
 {
@@ -23,69 +24,69 @@ namespace ImageViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly IImageUrlProvider _imageLinkProvider;
+
         public MainWindow()
         {
-            InitializeComponent();  
+            InitializeComponent();
+            _imageLinkProvider = new RedditPage(new Uri("http://www.reddit.com/r/aww"));
         }
 
         private void NextButton_Click(object sender, RoutedEventArgs e)
         {
-            var imgeUri = "http://i.imgur.com/NFCxi.jpg";
-           // var imgeUri = "http://www.sergojan.com/photos/jenn.jpg";
-            var targetFileName = "testimg.jpg";
 
             NextButton.IsEnabled = false;
 
-            // WORKS PERFECTLY, IS SYNCHRONOUS
-            using (WebClient webClient = new WebClient())
-            {
-                webClient.Proxy = null;  //avoids dynamic proxy discovery delay
-                webClient.CachePolicy = new RequestCachePolicy(RequestCacheLevel.Default);
-                try
-                {
-                    byte[] imageBytes = null;
-
-                    imageBytes = webClient.DownloadData(imgeUri);
-
-                    if (imageBytes == null)
-                    {
-                        return;
-                    }
-                    MemoryStream imageStream = new MemoryStream(imageBytes);
-                    BitmapImage image = new BitmapImage();
-
-                    image.BeginInit();
-                    image.StreamSource = imageStream;
-                    image.CacheOption = BitmapCacheOption.OnLoad;
-                    image.EndInit();
-
-                    image.Freeze();
-                    imageStream.Close();
-
-                    ImageBox.Source = image;
-                    NextButton.IsEnabled = true;
-                }
-                catch (WebException ex)
-                {
-                    //do something to report the exception
-                }
-            }
-
-
-
-            /*
-            // THIS WORKS PERFECTLY BUT IS ASYNC
+            /* 
+             // Async, stops working after a while.
             var image = new BitmapImage();
-
             image.BeginInit();
             image.DownloadCompleted += (a, b) =>
             {
-                ImageBox.Source = (BitmapImage)a; 
+                ImageBox.Source = (BitmapImage)a;
                 NextButton.IsEnabled = true;
             };
-            image.UriSource = new Uri(imgeUri);
+            image.UriSource = this._imageLinkProvider.GetRandomImageUrl();
             image.EndInit();*/
 
+
+            // Stuff to play with later.
+
+            //  var filePathSections = Assembly.GetEntryAssembly().Location.Split('\\');
+            // var fileName = String.Join("\\", filePathSections.Take(filePathSections.Count() - 1)) + "\\" + "redditsamplepageNew.txt";
+            //  var sampleRedditPageJson = Encoding.UTF8.GetString(File.ReadAllBytes(fileName));
+
+            //  var postUrls = GetPostUrlsFromRedditPage(sampleRedditPageJson);
+
+
+            //  Synchronously download the image from the given url.
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.Proxy = null; 
+                webClient.CachePolicy = new RequestCachePolicy(RequestCacheLevel.Default);
+                byte[] imageBytes = null;
+
+                imageBytes = webClient.DownloadData(this._imageLinkProvider.GetRandomImageUrl());
+
+                if (imageBytes == null)
+                {
+                    NextButton.IsEnabled = true;
+                    return;
+                }
+                MemoryStream imageStream = new MemoryStream(imageBytes);
+                BitmapImage image = new BitmapImage();
+
+                image.BeginInit();
+                image.StreamSource = imageStream;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.EndInit();
+
+                image.Freeze();
+                imageStream.Close();
+
+                ImageBox.Source = image;
+                NextButton.IsEnabled = true;
+            }
         }
     }
 }
